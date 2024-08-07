ca858344f7fed5d3cb1d701fc0da23c72e5331ee
Ignacho1830 <163877287+Ignacho1830@users.noreply.github.com>
2024-08-07 11:14:19 -0300

    Update Nachopracticouno.py

--- Nachopracticouno.py
+++ Nachopracticouno.py
@@ -1,9 +1,9 @@
 def pares():
- numeros_pares = []
- for i in range(10):
-    numero = int(input(f"Ingrese el numero : "))
-    if numero % 2 == 0:
-        numeros_pares.append(numero)
+    numeros_pares = []
+    for i in range(10):
+        numero = int(input(f"Ingrese el numero : "))
+        if numero % 2 == 0 and numero != 0:
+            numeros_pares.append(numero)
         print("Numeros pares ingresados son :", numeros_pares)
 
 pares()
